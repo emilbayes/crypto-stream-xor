6be3519dcb366d78bb821998964e403298bb895a
Emil Bay <github@tixz.dk>
2018-01-14 21:58:48 +0100

    Add and test constants

--- test.js
+++ test.js
@@ -1,18 +1,23 @@
-var s = require('.')
+var test = require('test')
+var csx = require('../')
 
-var state = Buffer.alloc(s.crypto_stream_xor_STATEBYTES)
-var key = Buffer.alloc(32).fill('hello world')
-var nonce = Buffer.alloc(24).fill('hi verden')
+test('constants', function (assert) {
+  assert.equal(typeof crypto_stream_xor_STATEBYTES, 'number')
+  assert.equal(typeof crypto_stream_xor_KEYBYTES, 'number')
+  assert.equal(typeof crypto_stream_xor_NONCEBYTES, 'number')
+  assert.equal(typeof crypto_stream_xsalsa20_xor_STATEBYTES, 'number')
+  assert.equal(typeof crypto_stream_xsalsa20_xor_KEYBYTES, 'number')
+  assert.equal(typeof crypto_stream_xsalsa20_xor_NONCEBYTES, 'number')
+  assert.equal(typeof crypto_stream_chacha20_xor_STATEBYTES, 'number')
+  assert.equal(typeof crypto_stream_chacha20_xor_KEYBYTES, 'number')
+  assert.equal(typeof crypto_stream_chacha20_xor_NONCEBYTES, 'number')
+  assert.equal(typeof crypto_stream_xchacha20_xor_STATEBYTES, 'number')
+  assert.equal(typeof crypto_stream_xchacha20_xor_KEYBYTES, 'number')
+  assert.equal(typeof crypto_stream_xchacha20_xor_NONCEBYTES, 'number')
+  assert.end()
+})
 
-var plain1 = Buffer.alloc(32).fill('emil er sej')
-var cipher = Buffer.alloc(32)
-var plain2 = Buffer.alloc(32)
 
-s.crypto_stream_xor_init(state, key, nonce)
-var state2 = Buffer.alloc(state.length).fill(state)
 
-s.crypto_stream_xor_update(state, cipher, plain1)
 
-s.crypto_stream_xor_update(state2, plain2, cipher)
 
-process.exit(plain1.compare(plain2))
